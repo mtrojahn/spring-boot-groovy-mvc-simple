683aa683baa360bc4ff2fec639acd97acdd394ad
Coderdood <mtrojahn@gmail.com>
2015-11-26 17:25:29 -0200

    small fix

--- src/main/groovy/com/mtrojahn/Application.groovy
+++ src/main/groovy/com/mtrojahn/Application.groovy
@@ -4,11 +4,9 @@ import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = ["com.mtrojahn", "com.mtrojahn.mvc"])
+@ComponentScan(basePackages = "com.mtrojahn.mvc")
 class Application {
     static main(args) {
         new SpringApplicationBuilder()
--- src/main/groovy/com/mtrojahn/mvc/MvcController.groovy
+++ src/main/groovy/com/mtrojahn/mvc/MvcController.groovy
@@ -4,15 +4,10 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
-/**
- * Created by trojahn on 11/24/15.
- */
-
 @Controller
 class MvcController {
-
     @RequestMapping(value = "/")
-    public @ResponseBody String hello() {
+    def @ResponseBody hello() {
         return "Hello World!"
     }
 }
